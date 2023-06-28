57d6634b078f22b0c8a5b1d461431dc0e51137ee
A l e x a n d r e <A l e x a n d r e@DESKTOP-URJU1R1>
2023-06-28 10:40:17 -0300

    Adicionar arquivos de projeto.

---
+++ RegistroFuncionarioApp/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroFuncionarioApp
+{
+    public class Funcionario
+    {
+        public int ID { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario) 
+        {
+            ID = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void Aumento(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+
+        public override string ToString() 
+        {
+            return ID + ", " + Nome + ", " + Salario;
+        }
+    }
+}
---
+++ RegistroFuncionarioApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using RegistroFuncionarioApp;
+using System.Globalization;
+using System.Collections.Generic;
+
+List<Funcionario> Lista = new List<Funcionario>();
+
+Console.WriteLine("Quantos empregados serão cadastrados?");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(n + "° Empregado:");
+    Console.Write("ID: ");
+    int ID = int.Parse(Console.ReadLine());
+
+    Console.Write("Nome: ");
+    string Nome = Console.ReadLine();
+
+    Console.Write("Salário: ");
+    double Salario = double.Parse(Console.ReadLine());
+
+    Lista.Add(new Funcionario(ID, Nome, Salario));
+}
+
+Console.Write("Qual funcionário você deseja aumentar o salário? ");
+int IDF = int.Parse(Console.ReadLine());
+
+Funcionario func = Lista.Find(x => x.ID == IDF);
+
+if (func != null)
+{
+    Console.Write("Qual é a porcentagem? ");
+    double porcentagem = double.Parse(Console.ReadLine());
+
+    func.Aumento(porcentagem);
+}
+else
+{
+    Console.WriteLine("ID não registrado");
+}
+
+foreach (Funcionario funcionario in Lista)
+{
+    Console.WriteLine(funcionario);
+}
+
+
